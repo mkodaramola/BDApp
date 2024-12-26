1f4d33272a15fbddc253542e53b4fbd055250ba4
Daramola Oluwafemi <mkodaramola@gmail.com>
2024-12-26 11:06:26 +0100

    Updated

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def login():
-    print("runnong")
+def index():
     return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
